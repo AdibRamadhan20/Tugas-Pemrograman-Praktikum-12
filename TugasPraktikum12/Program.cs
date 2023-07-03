e24395ca43d7d25ca2c57fca4cd131bc488e6dab
AdibRamadhan20 <adib2003@students.amikom.ac.id>
2023-07-04 00:35:12 +0700

    first commit

---
+++ TugasPraktikum12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasPraktikum12
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arrNim = { 1234, 1235, 1235 };
+			string[] arrNama = { "Erik", "Wely", "Sony" };
+			int[] arrNilai = { 75, 80, 95 };
+
+			Console.WriteLine("No.      Nim      Nama      Nilai");
+			Console.WriteLine("=================================");
+
+			for (int i = 0; i < arrNama.Length; i++)
+			{
+				Console.WriteLine("{0}.      {1}      {2}       {3}", i + 1, arrNim[i], arrNama[i], arrNilai[i]);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
